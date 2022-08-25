21a1bb43ddade5b881ca10505cc24e824670364e
Arad Koluok <44arad44@gmail.com>
2022-08-25 18:11:19 +0300

    open and read json file

--- main.py
+++ main.py
@@ -1,5 +1,51 @@
+import json
+
 def main():
-    pass
+    with open("test.json") as jsonFile:
+        jsonObject = json.load(jsonFile)
+        jsonFile.close()
+
+    name = jsonObject[0]['Name']
+    Contact = jsonObject[0]['Contact']
+    Email = jsonObject[0]['Email']
+    Hobbies = jsonObject[0]['Hobbies']
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
