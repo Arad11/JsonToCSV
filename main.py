8921f750c30dbdf8714987433a826efba4aca533
Arad Koluok <44arad44@gmail.com>
2022-08-25 23:16:34 +0300

    write data into csv

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import json
 import csv
+import sys
+
+jsonFilePath = sys[1] #example: D:\\projects\\JsonToCSV\\test.json
+csvFilePath = sys[2] #example: D:\\projects\\JsonToCSV\\copyToFile.csv
 
 def main():
-    with open("test.json") as jsonFile:
+    with open(jsonFilePath) as jsonFile:
         jsonObject = json.load(jsonFile)
         keys = getJsonKeysAsList(jsonObject)
         objects = getJsonAsList(jsonObject)
         jsonFile.close()
         
-    name = keys[0]
-    contact = keys[1]
-    email = keys[2]
-    hobbies = keys[3]
-
- 
+    with open(csvFilePath, 'w', encoding="UTF8") as csvFile:
+        writer = csv.DictWriter(csvFile, fieldnames=keys)
+        writer.writeheader()
+        writer.writerows(objects)
+    
 def getJsonKeysAsList(jsonObject):
     return list(jsonObject[0].keys())
 
@@ -22,39 +25,6 @@ def getJsonAsList(jsonObject):
     for object in jsonObject:
         objects.append(object)
     return objects
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
